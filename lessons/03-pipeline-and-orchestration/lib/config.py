948b7dfd0dfeca9a835473c11656319cedb355fb
palpi-to <pierre.alpi@artefact.com>
2025-10-20 13:24:26 +0200

    Update config.py
    Deleted if __name__ == "main.py" section

--- lessons/03-pipeline-and-orchestration/lib/config.py
+++ lessons/03-pipeline-and-orchestration/lib/config.py
@@ -6,10 +6,3 @@ CATEGORICAL_COLS = ["PULocationID", "DOLocationID", "passenger_count"]
 PROJECT_ROOT = Path(__file__).resolve().parents[3]
 DATA_DIRPATH = str(PROJECT_ROOT / "data")
 MODELS_DIRPATH = str(PROJECT_ROOT / "models")
-
-
-if __name__ == "__main__":
-    print("Current file:", Path(__file__).resolve())
-    print("Project root:", PROJECT_ROOT)
-    print("Data dir:", DATA_DIRPATH)
-    print("Models dir:", MODELS_DIRPATH)
